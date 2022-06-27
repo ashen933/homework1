63e954a83ee943dbbc9b7555b0257f66296c6f8e
ashen933 <90027273+ashen933@users.noreply.github.com>
2022-06-27 20:35:31 +0300

    Update work2.py

--- work2.py
+++ work2.py
@@ -1,19 +1,3 @@
 def int32_to_ip(int32):
-    a = 1
-    b = int32
-    c = []
-    u = 256
-    while a > 0 and b > u:
-
-        a = b % u
-        b = b // u
-        c.append(str(a))
-    c.append(str(b))
-    while len(c) < 4:
-        c.append('0')
-    return '.'.join(c[::-1])
-
-
-assert int32_to_ip(2154959208) == "128.114.17.104"
-assert int32_to_ip(0) == "0.0.0.0"
-assert int32_to_ip(2149583361) == "128.32.10.1"
+    r = str(int32//(256**3))+'.'+str((int32%(256**3))//(256**2))+'.'+str((int32%(256**2))//256)+'.'+str((int32%256))
+    return r
